ce96ebeb3eb4a97e718364af07bc4c25c74e7335
trevormahoney <trevormahoney12345@gmail.com>
2017-02-20 14:53:30 -0500

    encryption (dont know if it worked)

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,10 +1,13 @@
 var User = require('../models/user');
 
 exports.signup = function(req, res, next){
-	//1
+	
 	var email = req.body.email;
 	var password = req.body.password;
-	//2
+	
+	if( !email || !password){
+		return res.status(418).send({error: 'You must provide email and password.'})
+	}
 	User.findOne({ email: email }, function(err, existingUser){
 		if(err) {
 			return next(err);
@@ -14,7 +17,7 @@ exports.signup = function(req, res, next){
 			// return res.status(418).send("Email is in use");
 		}// Handles existing users
 	});
-	//3
+	
 	var user = new User ({
 		email: email,
 		password: password
@@ -22,13 +25,9 @@ exports.signup = function(req, res, next){
 	//To save the record to the DB.
 	user.save(function(err){
 		if(err) { return next(err); }
-		//4 Respond to requrest indicating the user was created
-		res.json(user);
+		//Respond to requrest indicating the user was created
+		// res.json(user);
+		res.json({success: true}); //make sure you have the {}
 	});
 }
 
-// exports.signup = function(req, res, next){
-// 	//Test
-// 	res.send("authorization is happening");
-// 	console.log(req.body);
-// }
--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 var userSchema = new Schema({
 	email: {
 		type: String,
@@ -9,5 +10,17 @@ var userSchema = new Schema({
 	password: String
 });
 var model = mongoose.model('user', userSchema);
+userSchema.pre('save', function(next){
+var user = this;
+	bcrypt.genSalt(10, function(err, salt){
+		if(err) { return next(err); }
+		bcrypt.hash(user.password, salt, null, function(err, hash){
+			if (err) { return next(err); }
+
+			user.password = hash;
+			next();
+		});
+	});
+});
 
 module.exports = model;
