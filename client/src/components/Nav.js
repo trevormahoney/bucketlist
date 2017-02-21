827c3dfa32b1255b4e167138eb8260c8478af590
trevormahoney <trevormahoney12345@gmail.com>
2017-02-21 13:34:28 -0500

    nav bar(i have originally put the nav bar in app.js instead of nav.js)

---
+++ client/src/components/Nav.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {Navbar, Nav, NavItem, NavDropdown, DropdownButton, MenuItem, CollapsibleNav} from 'react-bootstrap';
+
+class NavBarHeader extends Component {
+	render() {
+		return (
+			<Navbar>
+		    <Navbar.Header>
+		      <Navbar.Brand>
+		        <a href="#">React-Bootstrap</a>
+		      </Navbar.Brand>
+		    </Navbar.Header>
+		    <Nav>
+		      <NavItem eventKey={1} href="#">Sign In</NavItem>
+		      <NavItem eventKey={2} href="#">Sign Up</NavItem>
+		      <NavDropdown eventKey={3} title="Cool Stuff" id="basic-nav-dropdown">
+		        <MenuItem eventKey={3.1}>Action</MenuItem>
+		        <MenuItem eventKey={3.2}>Another action</MenuItem>
+		        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+		        <MenuItem divider />
+		        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+		      </NavDropdown>
+		    </Nav>
+		  </Navbar>
+		);
+	}
+
+}
+
+export default NavBarHeader;
--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import NavBarHeader from './nav';
 
 	export default class App extends Component {
   		render() {
     			return (
-      			<div>React Hello World</div>
+      			<div>
+      				<NavBarHeader />
+      			</div>
     			);
   		}
 	}
