b1361658023b76ccbfcaa892fcb73c52847061e2
trevormahoney <trevormahoney12345@gmail.com>
2017-02-21 12:40:30 -0500

    -

---
+++ client/src/components/5_ChildComponentsIntro.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class ChildComponentsIntro extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			joke: "Kenny G"
+
+		}
+	}
+	changeStateCustomMethod(event){
+		this.setState({joke: event.target.value})
+	}
+	render() {
+		return (
+			<div>
+				<h3>Child Componets</h3>
+				<p>Use the input field to see the state change.</p>
+					<p>{this.state.joke} is the greatest saxophone player of all time.</p>
+					<ChildComponent updateProps={this.changeStateCustomMethod.bind(this)} />
+			</div>
+		);
+	}
+}
+
+const ChildComponent = (props) =>
+	<input type="text" onChange={props.updateProps}/>
+
+export default ChildComponentsIntro
