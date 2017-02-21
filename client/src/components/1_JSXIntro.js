b26428c6e039675cd1d6685b1a9e43a089536f69
trevormahoney <trevormahoney12345@gmail.com>
2017-02-21 11:03:34 -0500

    test to make sure the localhost 8080 responds

---
+++ client/src/components/1_JSXIntro.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class JSXIntro extends React.Component {
+	render() {
+		return (
+			<div>
+				<h3>JSX</h3>
+				<ul>
+					<li>Stands for JavaScript Extensions</li>
+					<li>Looks a lot like HTML, but it's not.</li>
+					<li>Compiles down to pure JavaScript</li>
+					<li>Easer than wrting pure JS</li>
+					<li>Makes code safe from injection attacks</li>
+				</ul>
+				<hr/>
+			</div>
+		);
+	}
+}
+
+export default JSXIntro;
